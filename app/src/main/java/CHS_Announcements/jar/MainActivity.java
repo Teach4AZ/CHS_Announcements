dc527e597ba915150d7962daef367d75a5be68ee
Sam Alexander <sam@CTE.CHS>
2017-04-05 09:45:05 -0700

    WebView working 1.0

---
+++ app/src/main/java/CHS_Announcements/jar/MainActivity.java
@@ -0,0 +1,59 @@
+package CHS_Announcements.jar;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+    private Button newsletterButton;
+    WebView webView;
+    public void onCreate(Bundle savedInstanceState) {
+        final Context context = this;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        newsletterButton = (Button) findViewById(R.id.newsletterButton);
+
+        newsletterButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                showWebPage("https://www.smore.com/wdmg0");
+            }
+
+
+
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            if(webView != null)
+            {
+                setContentView(R.layout.main);
+                webView = null;
+            }
+            else this.finishActivity(0);
+        }
+    }
+
+    private void showWebPage(String url)
+    {
+        setContentView(R.layout.webview);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+    }
+
+}
