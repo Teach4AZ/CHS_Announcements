bd8bb6f3e3a91b1234fdc43bed5a63a7d1c072d8
Teach4AZ <Teach100!>
2017-04-05 11:09:05 -0700

    Both Newsletter and Announcements working

---
+++ app/src/main/java/CHS_Announcements/jar/AnnouncementsActivity.java
@@ -0,0 +1,24 @@
+package CHS_Announcements.jar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by sam on 4/5/2017.
+ */
+
+public class AnnouncementsActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://cusd80.com/site/default.aspx?PageType=14&DomainID=913&PageID=6389&ModuleInstanceID=10178&ViewID=606008db-225b-4ad2-8f7b-9ebac54372c1&IsMoreExpandedView=True");
+    }
+}
--- app/src/main/java/CHS_Announcements/jar/MainActivity.java
+++ app/src/main/java/CHS_Announcements/jar/MainActivity.java
@@ -12,8 +12,15 @@ import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+
     private Button newsletterButton;
+    private Button announcementsButton;
+    private Button teacherPagesButton;
+    private Button callOfficeButton;
+    private Button callAttendanceButton;
+
     WebView webView;
+
     public void onCreate(Bundle savedInstanceState) {
         final Context context = this;
 
@@ -21,13 +28,29 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
 
         newsletterButton = (Button) findViewById(R.id.newsletterButton);
-
+        announcementsButton = (Button) findViewById(R.id.announcementsButton);
+        teacherPagesButton = (Button) findViewById(R.id.teacherPagesButton);
+        callOfficeButton = (Button) findViewById(R.id.callOfficeButton);
+        callAttendanceButton = (Button) findViewById(R.id.callAttendanceButton);
         newsletterButton.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
 
                 Intent intent = new Intent(MainActivity.this,NewsletterActivity.class);
+                startActivity(intent);
+
+            }
+
+
+
+        });
+        announcementsButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(MainActivity.this,AnnouncementsActivity.class);
                 startActivity(intent);
 
             }
